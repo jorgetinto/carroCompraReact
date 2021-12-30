aaa5afc1fa5f413e45b55a263ce6f702c437194a
jpinogaray <jpino@hsalvador.cl>
2021-12-30 17:53:51 -0300

    se implementan cambios

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import { Component } from 'react';
 import Productos from './components/Productos';
 import Layout from './components/Layout';
+import Navbar from './components/Navbar';
 import Title from './components/Title';
 
 
@@ -11,17 +12,43 @@ class App extends Component{
       {name: 'Tomate', price: 1500, img:'/productos/tomate.jpg'},
       {name: 'Arbejas', price: 2500, img:'/productos/arbejas.jpg'},
       {name: 'Lechuga', price: 500, img:'/productos/lechuga.jpg'},
-    ]
+    ],
+    carro: []
   }
 
+  agregarAlCarro = (producto) => {
+
+    const {carro} = this.state;
+
+    if (carro.find(x => x.name === producto.name)) {
+      const newCarro = carro.map(x => x.name === producto.name
+                                      ? ({ 
+                                        ...x, 
+                                        cantidad: x.cantidad + 1 
+                                      })
+                                      : x )
+      return this.setState({carro: newCarro});
+    }
+
+
+    return this.setState({
+      carro: this.state.carro.concat({
+        ...this.props,
+        cantidad: 1,
+      })
+    })
+  }
 
   render() {
+    console.log(this.state.carro)
     return (
+     
       <div>
+        <Navbar carro={this.state.carro}/>
         <Layout>
           <Title />
           <Productos
-                    agregarAlCarro={() => console.log('No hace nada')}
+                    agregarAlCarro={this.agregarAlCarro}
                     productos = {this.state.productos}
                   />
         </Layout>
